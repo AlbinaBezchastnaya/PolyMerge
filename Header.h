4ed562f094f0a3980243d3965f7ebfcde45bd326
AlbinaBezchastnaya <Albina.Bezchastnaya@yandex.ru>
2025-01-21 15:16:02 +1000

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,6 @@
+#pragma once
+
+/* функция слияния записи элементов в исходный массив */
+void merge(int* arr, int l, int m, int r);
+/* рекурсивная функция сортировки */
+void mergeSort(int* arr, int l, int r);
